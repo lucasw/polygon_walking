6d1d634fe68e43e090851d33487ff8b8579b680d
lucasw <lucasw_backwards@gmail.com>
2014-02-28 02:26:47 -0800

    Rotating hexagon

--- polygonal_approximation_to_walking.py
+++ polygonal_approximation_to_walking.py
@@ -1,4 +1,16 @@
+import math
 import pygame
+
+def radially_symmetric_polygon(screen, color, sides, center, radius, offset):
+
+  arc = 2 * math.pi / sides
+  point_list = []
+  for i in range(sides):
+    x = center[0] + radius * math.cos(i * arc + offset) 
+    y = center[1] + radius * math.sin(i * arc + offset)
+    point_list.append( (x,y))
+
+  pygame.draw.polygon(screen, color, point_list)
 
 pygame.init()
 
@@ -9,10 +21,15 @@ screen = pygame.display.set_mode(size)
 black = 0,0,0
 white = 255,255,255
 
+rotation = 0
+
 while 1:
   
   screen.fill(white)
   pos = (int(width/2), int(height/2))
   radius = int(height/2.5)
   pygame.draw.circle(screen, black, pos, radius, 5)
+  radially_symmetric_polygon(screen, (128,128,128), 6, pos, radius, rotation)
   pygame.display.flip()
+  
+  rotation += 0.001
